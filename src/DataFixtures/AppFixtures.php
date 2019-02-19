6d53a0104bd3a7257101827a60c3f3f64621e355
Tiffany <linviia@hotmail.fr>
2019-02-19 09:25:44 +0100

    Ajout des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+
+// src/DataFixtures/AppFixtures.php
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+// ...
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // créer un utilisateur ayant un rôle admin 
+        $user = new User(); $user->setEmail('admin@example.com'); 
+        $user->setRoles(['ROLE_ADMIN']); 
+        // encoder le mot de passe 
+        $password = $this->encoder->encodePassword($user, '123456'); 
+        $user->setPassword($password); 
+        $manager->persist($user); 
+        // créer un student 
+        $user = new User(); 
+        $user->setEmail('toto.pop@example.com');
+        $password = $this->encoder->encodePassword($user, '123456'); 
+        $user->setPassword($password); 
+        $manager->persist($user);
         // $product = new Product();
         // $manager->persist($product);
 
