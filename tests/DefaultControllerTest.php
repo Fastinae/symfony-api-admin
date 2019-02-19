cb2b801ebb7ba5108f7337259848bca1e1d1745b
Tiffany <linviia@hotmail.fr>
2019-02-19 10:01:57 +0100

    Ajout des tests fonctionnels et leurs dépendances

---
+++ tests/DefaultControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+
+    public function testLoginPage()
+    {
+        // create Client
+        $client = static::createClient();
+        // Go to the 'URL `/login`
+        $crawler = $client->request('GET', '/login');
+        // Check that the server responds with an HTTP 200
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        // Check that the `Email` text is in the` body` tag
+        $this->assertContains('Email', $crawler->filter('body')->text());
+        // Check that the `Password` text is in the` body` tag
+        $this->assertContains('Password', $crawler->filter('body')->text());
+    }
+
+}
